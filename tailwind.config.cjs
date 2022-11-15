f69c8e63e1642c1c5993f28bcfdd658ed9477f71
fweth <klausgy@gmail.com>
2022-11-15 12:47:53 +0000

    ...

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
-const baseSpacing = Object.fromEntries(
+const basePixels = Object.fromEntries(
     [1, 2, 4, 8, 16, 32, 64]
         .map((i) => [1, 2, 3, 4, 5].map((j) => [i * j, `${4 * i * j}px`]))
         .flat()
 );
+const baseFractions = {
+    "1/8": "12.5%",
+    "1/4": "25%",
+    "3/8": "37.5%",
+    "1/2": "50%",
+    "5/8": "62.5%",
+    "3/4": "75%",
+    "7/8": "87.5%",
+};
 module.exports = {
     content: ["./src/**/*.astro"],
     theme: {
@@ -21,17 +30,24 @@ module.exports = {
             xl: "18px",
             xxl: "24px",
         },
+        spacing: { 0: "0", ...basePixels },
+    },
+    extend: {
+        colors: {
+            beige: "#66614e",
+            black: "#000",
+            red: "#b75450",
+        },
         margin: {
-            ...baseSpacing,
             s: "25px",
             m: "55px",
             l: "80px",
             xl: "105px",
         },
         minHeight: {
-            ...baseSpacing
+            ...basePixels,
         },
-        spacing: { 0: "0", ...baseSpacing },
+        width: baseFractions,
     },
     plugins: [],
 };
