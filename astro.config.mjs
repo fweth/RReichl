6fc4a569ad3603a41c84d33c29710189f8c5d896
Klaus Gy <klausgy@gmail.com>
2026-08-14 18:12:39 +0200

    feat: add main index and installations pages with content and images; update tsconfig for path aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,32 @@
 import { defineConfig } from "astro/config";
+import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
+import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
+const locales = {
+  en: "en-US",
+  de: "de-DE",
+};
+const defaultLocale = "en";
+
 // https://astro.build/config
 export default defineConfig({
-  i18n: {
-    locales: ["en", "de"],
-    defaultLocale: "en",
-  },
+  site: "https://rpreichl.com/",
   trailingSlash: "always",
+  integrations: [
+    i18n({
+      locales,
+      defaultLocale
+    }),
+    sitemap({
+      i18n: {
+        locales,
+        defaultLocale,
+      },
+      filter: filterSitemapByDefaultLocale({ defaultLocale }),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
